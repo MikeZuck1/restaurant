18250d72b8dc9b09162277f74a6410dc3a9afbdd
Mike Batelahoko <mikebatelahoko96@gmail.com>
2025-03-28 20:02:39 +0100

    added section informations.

--- app/informations/page.tsx
+++ app/informations/page.tsx
@@ -5,8 +5,8 @@ export default function Informations() {
     week: "Du Lundi à jeudi : 12h00-14h30 et 19h00-22h30",
     weekend: "Du Vendredi et Samedi : 12h00-14h30 et 19h00-23h30",
     sunday: "Du Dimache : 12h00-14h30 et 19h00-22h30",
-    adress: "",
-    sentence: "",
+    adress: "Notre adresse est le 1 rue des moineaux à Paris.",
+    sentence: "Trouvez votre itinéraire sur cette carte :",
   };
 
   return (
@@ -18,18 +18,18 @@ export default function Informations() {
         <div className="">
           <div className="bg-blue-400 text-sm mt-[3rem] h-[20rem] w-[25rem]">
             <h3 className="font-bold">{schedule.firstTitle}</h3>
-            <p className="">
-              <p className="my-7">{schedule.week}</p>
-              <p className="my-7">{schedule.weekend}</p>
-              <p className="my-7">{schedule.sunday}</p>
-            </p>
+            <hr className="my-6 border border-red-900 w-1/6" />
+            <p className="my-7">{schedule.week}</p>
+            <p className="my-7">{schedule.weekend}</p>
+            <p className="my-7">{schedule.sunday}</p>
           </div>
         </div>
         <div>
-          <div className="bg-blue-400 font-bold text-sm mt-[3rem] h-[30rem] w-[25rem]">
-            <h3>{schedule.secondTitle} :</h3>
-            <p className="my-7">{}</p>
-            <p className="my-7">{}</p>
+          <div className="bg-blue-400 text-sm mt-[3rem] h-[30rem] w-[25rem]">
+            <h3 className="font-bold">{schedule.secondTitle} :</h3>
+            <hr className="my-6 border border-red-900 w-1/6" />
+            <p className="my-7">{schedule.adress}</p>
+            <p className="my-7">{schedule.sentence}</p>
           </div>
         </div>
       </div>
